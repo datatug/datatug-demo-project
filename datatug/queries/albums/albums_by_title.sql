a3d0af6339e5294d241476b0f4cef9550885540e
Alexander Trakhimenok <alex@trakhimenok.com>
2021-03-24 20:38:04 +0000

    more entities

--- datatug/queries/albums/albums_by_title.sql
+++ datatug/queries/albums/albums_by_title.sql
@@ -7,5 +7,7 @@ SELECT
     (SELECT COUNT(DISTINCT GenreId) FROM Track AS t WHERE t.AlbumId = al.AlbumId) AS GenresCount
 FROM Album AS al
 INNER JOIN Artist AS ar ON ar.ArtistId = al.ArtistId
-WHERE al.ArtistId = IFNULL(@ArtistId, al.ArtistId)
+WHERE
+	al.AlbumId = IFNULL(@AlbumId, al.AlbumId)
+AND al.ArtistId = IFNULL(@ArtistId, al.ArtistId)
 ORDER BY Title
